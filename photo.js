dee311c1f23978312d2a8eed5bfedd64d68e0143
zhangto <48229084+zhangto@users.noreply.github.com>
2019-03-25 20:46:28 +0800

    js代码
    照片墙组件的js代码块

---
+++ photo.js
@@ -0,0 +1,51 @@
+var lis = {
+	template: '#lis',
+	data() {
+		const list = [{
+			image: ''
+		}];
+		return {
+			list,
+			waterfallW: 220, //图片div宽度
+			screenWidth: document.body.clientWidth //屏幕宽度
+		};
+	},
+	created() {
+		for (var i = 1; i < 23; i++) {
+			this.list.push({
+				image: 'image/0000000' + i + '.jpg'
+			});
+		}
+
+	},
+	mounted() {
+		var nodeList = this.$refs.col; //图片
+		this.doSort(nodeList);
+	},
+	methods: {
+		doSort: function(nodeList) {
+			var itemWidth = this.waterfallW;
+			var columns = parseInt(this.screenWidth / (itemWidth + gap));
+			var arr = [];
+			for (var i = 0; i < nodeList.length; i++) {
+				if (i < columns) {
+					nodeList[i].style.top = 0;
+					nodeList[i].style.left = (itemWidth + gap) * i + 'px';
+					arr.push(nodeList[i].offsetHeight);
+				} else {
+					var minHeight = arr[0];
+					var index = 0;
+					for (var j = 0; j < arr.length; j++) {
+						if (minHeight > arr[j]) {
+							minHeight = arr[j];
+							index = j;
+						}
+					}
+					nodeList[i].style.top = arr[index] + gap + 'px';
+					nodeList[i].style.left = nodeList[index].offsetLeft + 'px';
+					arr[index] = arr[index] + nodeList[i].offsetHeight + gap;
+				}
+			}
+		}
+	}
+};
